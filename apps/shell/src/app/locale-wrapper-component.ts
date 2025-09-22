8d52a255ef10faa9cfc4d96c17f211b78a6ac169
Ichiroku <danieliyo65@gmail.com>
2025-09-22 09:58:38 +0200

    fix: added destroyRef to locale wrapper even though it shouldnt be needed

--- apps/shell/src/app/locale-wrapper-component.ts
+++ apps/shell/src/app/locale-wrapper-component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { RokuTranslator } from '@portfolio/localization/rokutranslator';
@@ -19,35 +19,41 @@ const SUPPORTED_LOCALES = ['en', 'es'];
 export class LocaleWrapperComponent {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
+  private _destroyRef = inject(DestroyRef);
 
   constructor() {
-    this.route.paramMap.pipe(takeUntilDestroyed()).subscribe(async (params) => {
-      const originalLocale = params.get('locale');
-      let locale = originalLocale;
+    this.route.paramMap
+      .pipe(takeUntilDestroyed(this._destroyRef))
+      .subscribe(async (params) => {
+        const originalLocale = params.get('locale');
+        let locale = originalLocale;
 
-      if (!locale) {
-        locale = RokuTranslator.getLocale();
-      }
+        if (!locale) {
+          locale = RokuTranslator.getLocale();
+        }
 
-      if (!locale || !SUPPORTED_LOCALES.includes(locale)) {
-        locale = 'en';
-      }
+        if (!locale || !SUPPORTED_LOCALES.includes(locale)) {
+          locale = 'en';
+        }
 
-      if (locale !== originalLocale) {
-        // Rebuild the URL, replace first segment (locale) only
-        const tree = this.router.createUrlTree(
-          [locale, ...this.route.snapshot.url.map((seg) => seg.path).slice(1)],
-          {
-            queryParams: this.route.snapshot.queryParams,
-            fragment: this.route.snapshot.fragment || undefined,
-          }
-        );
+        if (locale !== originalLocale) {
+          // Rebuild the URL, replace first segment (locale) only
+          const tree = this.router.createUrlTree(
+            [
+              locale,
+              ...this.route.snapshot.url.map((seg) => seg.path).slice(1),
+            ],
+            {
+              queryParams: this.route.snapshot.queryParams,
+              fragment: this.route.snapshot.fragment || undefined,
+            }
+          );
 
-        // Navigate without reloading the whole app
-        this.router.navigateByUrl(tree, { replaceUrl: true });
-      } else {
-        RokuTranslator.changeLocale(locale);
-      }
-    });
+          // Navigate without reloading the whole app
+          this.router.navigateByUrl(tree, { replaceUrl: true });
+        } else {
+          RokuTranslator.changeLocale(locale);
+        }
+      });
   }
 }
