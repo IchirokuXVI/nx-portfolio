cd7a0d0654000632e80f7a58272334c3836de14b
Ichiroku <danieliyo65@gmail.com>
2026-05-26 10:50:51 +0200

    Started header, added navigation and business logo

--- libs/damoclesSword/ui/src/lib/main-header/main-header.ts
+++ libs/damoclesSword/ui/src/lib/main-header/main-header.ts
@@ -1,9 +1,16 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'lib-damoclesSword-main-header',
-  imports: [],
+  imports: [AsyncPipe, RouterModule],
   templateUrl: './main-header.html',
   styleUrl: './main-header.scss',
 })
-export class MainHeader {}
+export class MainHeader {
+  // @ts-expect-error TypeScript cannot resolve dynamic imports with relative paths in module federation setup
+  damoclesLogo = import(`../../../assets/damoclesSwordLogo.svg`).then(
+    (m) => m.default
+  );
+}
