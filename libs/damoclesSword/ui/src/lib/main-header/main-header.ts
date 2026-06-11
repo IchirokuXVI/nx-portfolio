e9e0a074af622ad51dc88dc462b6923829bd5ad1
Ichiroku <danieliyo65@gmail.com>
2026-06-11 12:24:25 +0200

    Moved nav links static data to feature shell

--- libs/damoclesSword/ui/src/lib/main-header/main-header.ts
+++ libs/damoclesSword/ui/src/lib/main-header/main-header.ts
@@ -25,24 +25,7 @@ export class MainHeader {
   selectedLanguage = input<string>('en');
   languageChange = output<string>();
 
-  navLinks = [
-    {
-      label: 'header.home',
-      url: ['./'],
-    },
-    {
-      label: 'header.services',
-      url: ['./services'],
-    },
-    {
-      label: 'header.about',
-      url: ['./about'],
-    },
-    {
-      label: 'header.contact',
-      url: ['./contact'],
-    },
-  ];
+  navLinks = input<{ label: string; url: string[] }[]>([]);
 
   _rokuTranslatorServ = inject(RokuTranslatorService);
 
